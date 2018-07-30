2ffdb67555b92710b5ca37030f0e98e23778e63b
Sofiene Trabelsi <sofiene@ALPHA.DEEPERA>
2018-07-30 15:30:51 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServeurCreateStockApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurCreateStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurCreateStockApplication.class, args);
+	}
+}
